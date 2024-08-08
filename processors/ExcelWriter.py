97cefd0e0a7710f380a6484861ba46109d506d58
Stefano <fortu.ste@hotmail.it>
2024-08-08 22:51:01 +0200

    openpyxl introduction pt2

--- processors/ExcelWriter.py
+++ processors/ExcelWriter.py
@@ -17,9 +17,9 @@ class ExcelWriter:
 
         self.wb = openpyxl.load_workbook(self.filename_in)
         self.ws = self.wb.get_sheet_by_name(self.sheetname)
-        #self.ws.outline_settings(symbols_below=False)
-        #self.writer = pd.ExcelWriter(self.filename_in, engine='xlsxwriter')
+        self.ws.sheet_properties.outlinePr.summaryBelow = False
 
+        # self.writer = pd.ExcelWriter(self.filename_in, engine='xlsxwriter')
 
     def create_output_name(self):
         filename_in_no_extension = self.filename_in.split(".")[0]
@@ -31,9 +31,8 @@ class ExcelWriter:
         return filename_out
 
     def __del__(self):
-        #self.ws.autofit()
-        #self.wb.close()
-        pass
+        self.wb.save(self.filename_out)
+        self.wb.close()
 
     def process(self, main_category_results, group_results):
         if not isinstance(main_category_results, CategoryResults):
@@ -48,56 +47,67 @@ class ExcelWriter:
         _out_column = 3
         _savings_column = 4
 
-        row_num = 1
-        self.ws.cell(row=row_num, column=_category_column).value  ="Categories"
+        row_num = 50
+        self.ws.cell(row=row_num, column=_category_column).value = "Categories"
         self.ws.cell(row_num, _in_column, "in")
         self.ws.cell(row_num, _out_column, "out")
         self.ws.cell(row_num, _savings_column, "savings")
         row_num += 1
 
-        print(self.filename_out)
-        self.wb.save(self.filename_out)
-        self.wb.close()
-        exit()
-        return
-        for main_cat in list(CategoryStructure.categories.keys()):
-            self.ws.write(row_num, 0, main_cat)
-            self.ws.write(row_num, 1, main_category_results.df.loc[main_cat]["in"])
-            self.ws.write(row_num, 2, main_category_results.df.loc[main_cat]["out"])
-            self.ws.write(row_num, 3, main_category_results.df.loc[main_cat]["savings"])
-            self.ws.set_row(row_num, None, None, {'collapsed': True})
-            row_num += 1
-            for cat in list(CategoryStructure.categories[main_cat]):
-                self.ws.write(row_num, 0, cat)
-                self.ws.write(row_num, 1, main_category_results.df.loc[cat]["in"])
-                self.ws.write(row_num, 2, main_category_results.df.loc[cat]["out"])
-                self.ws.write(row_num, 3, main_category_results.df.loc[cat]["savings"])
-                self.ws.set_row(row_num, None, None, {'level': 1, 'hidden': True})
-                row_num += 1
+        excel_structure = {"level1": [], "level2": []}
 
-        self.ws.write(row_num, 0, "-")
-        self.ws.write(row_num, 1, "-")
-        self.ws.write(row_num, 2, "-")
-        self.ws.write(row_num, 3, "-")
-        row_num += 1
-        for main_group in list(CategoryStructure.expense_groups.keys()):
-            self.ws.write(row_num, 0, main_group)
-            self.ws.write(row_num, 1, group_results.df.loc[main_group]["in"])
-            self.ws.write(row_num, 2, group_results.df.loc[main_group]["out"])
-            self.ws.write(row_num, 3, group_results.df.loc[main_group]["savings"])
-            self.ws.set_row(row_num, None, None, {'collapsed': True})
+        for main_cat in list(CategoryStructure.categories.keys()):
+            self.ws.cell(row_num, _category_column, main_cat)
+            self.ws.cell(row_num, _in_column, main_category_results.df.loc[main_cat]["in"])
+            self.ws.cell(row_num, _out_column, main_category_results.df.loc[main_cat]["out"])
+            self.ws.cell(row_num, _savings_column, main_category_results.df.loc[main_cat]["savings"])
             row_num += 1
-            for sub_group in CategoryStructure.expense_groups[main_group].keys():
-                self.ws.write(row_num, 0, sub_group)
-                self.ws.write(row_num, 1, group_results.df.loc[sub_group]["in"])
-                self.ws.write(row_num, 2, group_results.df.loc[sub_group]["out"])
-                self.ws.write(row_num, 3, group_results.df.loc[sub_group]["savings"])
-                self.ws.set_row(row_num, None, None, {'level': 1, 'hidden': True})
+            start_group_level_1 = row_num
+            for cat in list(CategoryStructure.categories[main_cat]):
+                self.ws.cell(row_num, _category_column, cat)
+                self.ws.cell(row_num, _in_column, main_category_results.df.loc[cat]["in"])
+                self.ws.cell(row_num, _out_column, main_category_results.df.loc[cat]["out"])
+                self.ws.cell(row_num, _savings_column, main_category_results.df.loc[cat]["savings"])
+                end_group_level_1 = row_num
                 row_num += 1
+            excel_structure["level1"].append((start_group_level_1, end_group_level_1))
+
+        for t in excel_structure["level1"]:
+            self.ws.row_dimensions.group(t[0], t[1], hidden=True, outline_level=1)
+
+        self.ws.cell(row_num, _category_column, "-")
+        self.ws.cell(row_num, _in_column, "-")
+        self.ws.cell(row_num, _out_column, "-")
+        self.ws.cell(row_num, _savings_column, "-")
+
+        excel_structure = {"level1": [], "level2": []}
+        row_num += 1
+        for main_group in list(CategoryStructure.expense_groups.keys())[1:]:
+            self.ws.cell(row_num, _category_column, main_group)
+            self.ws.cell(row_num, _in_column, group_results.df.loc[main_group]["in"])
+            self.ws.cell(row_num, _out_column, group_results.df.loc[main_group]["out"])
+            self.ws.cell(row_num, _savings_column, group_results.df.loc[main_group]["savings"])
+            row_num += 1
+            start_group_level_1 = row_num
+            for sub_group in CategoryStructure.expense_groups[main_group].keys():
+                self.ws.cell(row_num, _category_column, sub_group)
+                self.ws.cell(row_num, _in_column, group_results.df.loc[sub_group]["in"])
+                self.ws.cell(row_num, _out_column, group_results.df.loc[sub_group]["out"])
+                self.ws.cell(row_num, _savings_column, group_results.df.loc[sub_group]["savings"])
+                row_num += 1
+                start_group_level_2 = row_num
                 for cat in list(CategoryStructure.expense_groups[main_group][sub_group]):
-                    self.ws.write(row_num, 0, cat)
-                    self.ws.write(row_num, 1, group_results.df.loc[cat]["in"])
-                    self.ws.write(row_num, 2, group_results.df.loc[cat]["out"])
-                    self.ws.write(row_num, 3, group_results.df.loc[cat]["savings"])
-                    self.ws.set_row(row_num, None, None, {'level': 2, 'hidden': True})
+                    self.ws.cell(row_num, _category_column, cat)
+                    self.ws.cell(row_num, _in_column, group_results.df.loc[cat]["in"])
+                    self.ws.cell(row_num, _out_column, group_results.df.loc[cat]["out"])
+                    self.ws.cell(row_num, _savings_column, group_results.df.loc[cat]["savings"])
+                    end_group_level_1 = row_num
+                    end_group_level_2 = row_num
                     row_num += 1
+                excel_structure["level2"].append((start_group_level_2, end_group_level_2))
+            excel_structure["level1"].append((start_group_level_1, end_group_level_1))
+
+        for t in excel_structure["level1"]:
+            self.ws.row_dimensions.group(t[0], t[1], hidden=True, outline_level=1)
+        for t in excel_structure["level2"]:
+            self.ws.row_dimensions.group(t[0], t[1], hidden=True, outline_level=2)
